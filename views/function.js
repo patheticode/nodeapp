8cb4fd30f582b4ec690fcfc3c8e94b4032a57a31
patheticode <patryk.senska@wp.pl>
2022-08-20 20:30:11 +0200

    popup

---
+++ views/function.js
@@ -0,0 +1,39 @@
+//funkcja aktywujaca okienko
+function popupToggle(){
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+    var blur = document.getElementById('mainContener')
+    blur.classList.toggle('active')
+}
+
+//funkcja blurujaca background
+
+
+
+
+
+
+
+
+//do bazy mongodb lub postgresql
+var chatMain = {};
+
+chatMain.opcje = ['zaloguj', 'zarejestruj', 'chat', 'sklep', 'kontakt'];
+
+var chatShop =  {
+
+    colors : ['purple', 'blackyellow'],
+    emoji : true,
+    premium: true,
+    user: 5,
+    userPre : 10,
+    checkCost : function(vat) {
+        return this.userPre - this.user + vat;
+    }
+}
+
+var kolor = chatShop.colors[0];
+
+console.log(kolor);
+console.log(chatMain.opcje[3]);
+console.log(chatShop.checkCost(3));
